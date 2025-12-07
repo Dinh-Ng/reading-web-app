a17d6f1ac4f86e4f605dea4d748dd7d48e1940c7
DiNg <dinhquangnguyen1997@gmail.com>
2025-12-07 11:17:50 +0700

    feat: replace hardcoded Firebase configuration with environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,23 +2,14 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyASFwpj_CYL_opCvgCKdMCdoO27YM4ICTQ",
-  authDomain: "reading-app-bdf29.firebaseapp.com",
-  projectId: "reading-app-bdf29",
-  storageBucket: "reading-app-bdf29.firebasestorage.app",
-  messagingSenderId: "641330265952",
-  appId: "1:641330265952:web:0fde6d7b780be23c92a805",
-  measurementId: "G-9SECSDPC5P"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
